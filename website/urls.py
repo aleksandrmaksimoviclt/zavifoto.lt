c95a447b6def185998496e0ce5bae3101ec2950a
Aleksandr Maksimovic <aleksandr.maksimoviclt@gmail.com>
2016-05-02 19:15:14 +0300

    configured project

---
+++ website/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+from . import views
+
+urlpatterns = [
+	url(r'^$', views.index, name='index'),
+	# url(r'^about$', views.about, name='about'),
+	# url(r'^contact-us$', views.contact, name='contact-us'),
+	# url(r'^projects/$', views.projects, name='projects'),
+	
+	# # projects urls
+
+	# url(r'^projects/(?P<name>[-\w]+)/$', views.project),
+	# url(r'^(?P<language>[-\w]+)/$', views.change_language)
+]
