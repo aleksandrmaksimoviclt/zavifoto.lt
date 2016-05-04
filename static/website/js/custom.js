ea8cd0d05c88a9e7240d50f3c342dcfeabcbb8a2
Aleksandr Maksimovic <aleksandr.maksimoviclt@gmail.com>
2016-05-04 21:18:04 +0300

    Started Sidebar styling

--- static/website/js/custom.js
+++ static/website/js/custom.js
@@ -1,3 +1,27 @@
 $( document ).ready(function() {
+	var ww = window.innerWidth;
 
+	if (ww > 768) {
+	    console.log('true');
+	} else {
+	    console.log('false');
+	    $('#wrapper').toggleClass("toggled");
+	    $('#hambrger').removeClass("is-active");
+
+	    
+	}
+
+	var $hamburger = $(".hamburger");
+  	$hamburger.on("click", function(e) {
+    	$hamburger.toggleClass("is-active");
+    	e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+  });
 });
+
+// 		$('#sidebar-wrapper').css("margin-left", "-100%");
+// 	    $('#sidebar-wrapper').css("left", "100%");
+// 	    $('#sidebar-wrapper').css("width", "100%");
+//    .sidebar-nav {
+//     width: 100%;
+// }
