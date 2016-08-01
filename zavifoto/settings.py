5a5cdb54842b591dd5793e698629c11363812cf0
lukas <lukas.dambr@gmail.com>
2016-08-01 18:03:44 +0300

    settings

--- zavifoto/settings.py
+++ zavifoto/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'esakw6i7=&%fjw7_2a1mw%k%k)oh=r$^*nw68%hj8#4)&^!4=5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
