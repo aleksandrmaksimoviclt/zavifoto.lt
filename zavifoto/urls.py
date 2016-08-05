2a4768665157d7ac77589d8b8ab88b4071f23a4c
lukas <lukas.dambr@gmail.com>
2016-08-05 10:37:20 +0300

    if debug check added

--- zavifoto/urls.py
+++ zavifoto/urls.py
@@ -2,7 +2,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.contrib import admin
-from zavifoto import settings
+from django.conf import settings
 
 
 
@@ -13,6 +13,7 @@ urlpatterns = [
 
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += patterns('',
-  url(r'^plate/', include('django_spaghetti.urls')),
-)
+if settings.DEBUG:
+	urlpatterns += patterns('',
+	  url(r'^plate/', include('django_spaghetti.urls')),
+	)
